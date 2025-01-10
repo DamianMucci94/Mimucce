a48dea7b1abdc1c527a2f8fe8b5898cee02ddf53
Damian Mucci <damianmucci@hotmail.com>
2025-01-10 09:20:49 -0300

    feat: Mimucce

--- mimucce/productos/models.py
+++ mimucce/productos/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
 
     name = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='img')
+    image = models.ImageField(upload_to='productos')
     is_relevant = models.BooleanField(default=False)
     category = models.CharField(max_length=10, choices=CATEGORIAS)
     price = models.DecimalField(max_digits=10, decimal_places=2)
